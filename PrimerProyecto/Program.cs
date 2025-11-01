23e956277891e8b1df1372b2f2ac70cab4ea865a
AxelRivera504 <125411077+AxelRivera504@users.noreply.github.com>
2025-11-01 17:13:25 -0600

    Clase-01/11/2025

---
+++ PrimerProyecto/Program.cs
@@ -0,0 +1,56 @@
+﻿//Versión 
+//namespace MiApp
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//        }
+//    }
+//}
+
+//Versión top level statement
+//Imprimi mensaje en consola.
+Console.WriteLine("Hello, World!");
+Console.Write("Hola Mundo, que tal");
+
+
+//Variables y tipos de datos
+
+//Numeros
+int numero1 = 0;
+double numero2 = 0;
+float numero3 = 0;
+decimal numero4 = 0;
+long numero5 = 0;
+
+//string, puede ser un array de chars
+string mensaje = "hola";
+
+//este solo almacena un caracter
+char valor = '0';
+
+//bool, true o false
+bool esVerdadero = false;
+bool esFalso = true;
+
+//list
+List<double> doubles = new List<double>();
+doubles.Add(numero1);
+doubles.Remove(numero2);
+double.Sqrt(numero3);
+
+List<double> double1 = new List<double>();
+double1.Add(numero1);
+double1.Add(numero1);
+double1.Add(numero1);
+
+doubles.AddRange(double1);
+//a = 1
+// b= 1,3,7,4,10
+
+//arrays
+int[] numeros = { 1, 2, 45, 6, 1 };
+string[] textos =  new string[6];
+string[] textos2;
