88b22070db69b376461970c9fc07c53522a98181
AxelRivera504 <125411077+AxelRivera504@users.noreply.github.com>
2025-11-22 18:09:57 -0600

    Clase 3 22_11_2025

---
+++ PrimerProyecto/Clases/Class3/Persona.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto.Clases.Class3
+{
+    public class Persona
+    {
+        #region Propiedades
+        public string NombreCompleto { get; set; } = string.Empty;
+        public int Edad { get; set; }
+        public decimal Altura { get; set; }
+        public decimal Peso { get; set; }
+        #endregion
+
+        #region Constructores
+        public Persona(string nombreCompleto, int edad, decimal altura, decimal peso)
+        {
+            NombreCompleto = nombreCompleto;
+            Edad = edad;
+            Altura = altura;
+            Peso = peso;
+        }
+        #endregion
+
+        #region Métodos
+        public void Caminar()
+        {
+            Console.WriteLine(NombreCompleto + "esta caminando" + Edad + "nose");
+            Console.WriteLine($"{NombreCompleto} esta caminando {Edad} nose");
+        }
+
+        public void Saludar()
+        {
+            Console.WriteLine($"{NombreCompleto} esta saludndo");
+        }
+
+        public double CalcularIMC()
+        {
+            double IMC = Convert.ToDouble(Peso) / Math.Sqrt(Convert.ToDouble(Altura));
+            return IMC;
+           
+        }
+        #endregion
+    }
+}
--- PrimerProyecto/Program.cs
+++ PrimerProyecto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using PrimerProyecto.Clases;
+using PrimerProyecto.Clases.Class3;
 //Versión 
 //namespace MiApp
 //{
@@ -44,5 +45,95 @@ Class2 class2 = new Class2();
 
 //Ciclos
 //class2.Ciclos();
+
+#endregion
+
+#region Clase 3 del 22 de noviembre del 2025
+/* 
+ Objetivos de la clase
+-Clases
+-Objetos
+-Constructores
+-Métodos
+-Propiedades
+-Encapsulamineto básico
+ */
+
+//Persona 1 Axel Rivera
+//Persona persona = new Persona(string.Empty, 0 ,0,0);
+//Console.WriteLine(persona.Altura);
+//Console.WriteLine(persona.Edad);
+//Console.WriteLine(persona.Peso);
+//Console.WriteLine(persona.NombreCompleto);
+
+//persona.Altura = 1.71m;
+//persona.Edad = 23;
+//persona.Peso = 1.75m;
+//persona.NombreCompleto = "Axel Rivera";
+//double IMC = persona.CalcularIMC();
+//Console.WriteLine($"El imc de {persona.NombreCompleto} es de: {IMC}");
+//Console.WriteLine(persona.Altura);
+//Console.WriteLine(persona.Edad);
+//Console.WriteLine(persona.Peso);
+//Console.WriteLine(persona.NombreCompleto);
+
+
+////Persona2 Alex Rivera
+//Persona persona2 = new Persona(string.Empty, 0, 0, 0);
+//persona2.Altura = 168m;
+//persona2.Edad = 23;
+//persona2.Peso = 175m;
+//persona2.NombreCompleto = "Alex Rivera";
+
+//Console.WriteLine(persona2.Altura);
+//Console.WriteLine(persona2.Edad);
+//Console.WriteLine(persona2.Peso);
+//Console.WriteLine(persona2.NombreCompleto);
+
+//Console.WriteLine("\n \n \n");
+//Console.WriteLine("===============================================================");
+
+////Grupo de personas
+//List<Persona> personas = new List<Persona>();
+
+//Manera 1 de agregar a una lista del tipo de una clase
+//Persona JuanCenteno = new Persona(string.Empty, 0, 0, 0);
+//JuanCenteno.Altura = 1.65m;
+//JuanCenteno.Edad = 24;
+//JuanCenteno.Peso = 1.80m;
+//JuanCenteno.NombreCompleto = "Juan Centeno";
+//personas.Add(JuanCenteno);
+
+//Persona EderSanchez = new Persona(string.Empty, 0, 0, 0);
+//EderSanchez.Altura = 1.72m;
+//EderSanchez.Edad = 24;
+//EderSanchez.Peso = 1.70m;
+//EderSanchez.NombreCompleto = "Eder Sanchez";
+//personas.Add(EderSanchez);
+
+//Manera 2 de agregar a una lista del tipo de una clase
+//personas.Add(new Persona(string.Empty, 0, 0, 0) {NombreCompleto = "Eder Sanchez", Altura = 1.71m, Edad = 24, Peso = 1.70m });
+
+// Recorrer las propiedades de la clase personas
+//foreach (var item in personas)
+//{
+//    Console.WriteLine(item.NombreCompleto);
+//    Console.WriteLine(item.Edad);
+//    Console.WriteLine(item.Altura);
+//    Console.WriteLine(item.Peso);
+//    Console.WriteLine("\n");
+//}
+
+//Ejercicio
+/*
+Una clase Estudiante
+Con propiedades: Nombre, Carrera, Promedio
+Un método: MostrarInfo()
+Un método: Aprobo() que retorne si aprobó (>=70)
+*/
+
+//dario.rivera@unah.hn
+//<3
+Console.ReadKey();
 
 #endregion
